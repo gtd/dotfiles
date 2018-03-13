3261021e5569ad482127e846b9a4e17a85eb85f8
Gabe da Silveira <gabe@websaviour.com>
2018-03-12 20:20:31 -0700

    Add gitconfig

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s $DOTFILES_ROOT/zshrc ~/.zshrc
 ln -s $DOTFILES_ROOT/ackrc ~/.ackrc
 ln -s $DOTFILES_ROOT/vimrc ~/.vimrc
 ln -s $DOTFILES_ROOT/gvimrc ~/.gvimrc
+ln -s $DOTFILES_ROOT/gitconfig ~/.gitconfig
