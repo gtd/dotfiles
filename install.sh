5061dc31081292c0974ac5f44c178386ad03cb10
Gabe da Silveira <gabe@websaviour.com>
2022-07-20 14:05:30 -0500

    Symlink in global gitignore

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ ln -sv $DOTFILES_ROOT/ackrc ~/.ackrc
 ln -sv $DOTFILES_ROOT/vimrc ~/.vimrc
 ln -sv $DOTFILES_ROOT/gvimrc ~/.gvimrc
 ln -sv $DOTFILES_ROOT/gitconfig ~/.gitconfig
+ln -sv $DOTFILES_ROOT/gitignore_global ~/.gitignore_global
 
 mkdir -p ~/.vim/autoload
 ln -sv $DOTFILES_ROOT/plug.vim ~/.vim/autoload/plug.vim
